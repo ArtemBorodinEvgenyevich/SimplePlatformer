43bd503efec07f06daebc6110a0fffa8655bd8e8
Lev <lkorasik83@gmail.com>
2020-07-16 14:17:15 +0500

    Add Level.cpp and Level.h

---
+++ include/Level.h
@@ -0,0 +1,17 @@
+//
+// Created by lkora on 16.07.2020.
+//
+
+#ifndef SIMPLEPLATFORMER_LEVEL_H
+#define SIMPLEPLATFORMER_LEVEL_H
+
+
+class Level {
+private:
+public:
+    Level();
+    ~Level();
+};
+
+
+#endif //SIMPLEPLATFORMER_LEVEL_H
---
+++ src/Level.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lkora on 16.07.2020.
+//
+
+#include "Level.h"
+
+Level::Level()
+{
+
+}
+
+Level::~Level()
+{
+
+}
